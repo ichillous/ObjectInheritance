862c780aadff69ed82de185056d3055d9405ac6c
ichillous <44874147+ichillous@users.noreply.github.com>
2024-05-29 13:16:43 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main extends Object{
+    public static void main(String[] args) {
+
+        Student max = new Student("Max", 21);
+        System.out.println(max);
+
+        PrimarySchoolStudent jimmy = new PrimarySchoolStudent("Jimmy", 8, "Carol");
+        System.out.println(jimmy);
+    }
+}
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student( String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+//    @Override
+//    public String toString() {
+//        return super.toString();
+//    }
+
+    @Override
+    public String toString() {
+        return name + " is " + age;
+//        return "Student{" +
+//                "name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+    }
+}
+
+class PrimarySchoolStudent extends Student {
+    private String parentName;
+
+    PrimarySchoolStudent(String name, int age, String parentName){
+        super(name, age);
+        this.parentName = parentName;
+    }
+
+    @Override
+    public String toString() {
+        return parentName + "'s kid, " + super.toString();
+    }
+}
